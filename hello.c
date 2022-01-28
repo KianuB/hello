c389368f3988a636778f0c787abc08d9852f0d65
Kianu Benigno <kcbenigno15@gmail.com>
2022-01-28 16:39:22 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Kianu Benigno
+ * Partner: Natalie Ryan
+ *         kbenigno@unl.edu
+ * Date: 2022/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kianu Benigno and Natalie Ryan\n");
+  printf("Major: Computer Engineering\n");
+
+  return 0;
+}
